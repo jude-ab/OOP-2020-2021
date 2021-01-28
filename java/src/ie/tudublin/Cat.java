2b18edc5338245b9cbb091a50a9b1282466db24e
jude-ab <73896237+jude-ab@users.noreply.github.com>
2021-01-28 18:46:41 +0000

    hello

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -30,35 +30,4 @@ public class Cat extends Animal
         }
     }
 
-    private int numLives; 
-
-    public int getnumLives()
-    {
-        return numLives;
-    }
-
-    public void setnumLives(int numLives)
-    {
-        this.numLives = numLives;
-    }
-
-    public Cat(int numLives)
-    {
-        this.numLives = 9; 
-    }
-
-    static void kill()
-    {
-        if(numLives > 0)
-        {
-          numlives - 1;  
-          System.out.println("Ouch!"); 
-        }
-        else
-        {
-            System.out.println("Dead")
-        }
-    }
-
-    Cat ginger = new Cat("ginger")
 }
